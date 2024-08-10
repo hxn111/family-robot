f99ba2b7c311badcbdc49e0acdc98ccd17f9a3a3
hxn111 <418459483@qq.com>
2024-08-11 04:21:44 +0800

    hat_arm

---
+++ hat_arm.py
@@ -0,0 +1,51 @@
+from robot_hat import Servo
+import time
+
+servo1 = Servo("P0")
+servo2 = Servo("P4")
+
+servo1.angle(0)
+servo2.angle(0)
+
+def happy_mode():
+    for i in range(0,90,2):
+        servo1.angle(i)
+        servo2.angle(-i)
+        time.sleep(0.01)
+
+    for i in range(90,30,-1):
+        servo1.angle(i)
+        servo2.angle(-i)
+        time.sleep(0.01)
+
+    for i in range(30,90,1):
+        servo1.angle(i)
+        servo2.angle(-i)
+        time.sleep(0.01)
+
+    for i in range(90,30,-1):
+        servo1.angle(i)
+        servo2.angle(-i)
+        time.sleep(0.01)
+
+    for i in range(30,90,1):
+        servo1.angle(i)
+        servo2.angle(-i)
+        time.sleep(0.01)
+
+    for i in range(90,0,-1):
+        servo1.angle(i)
+        servo2.angle(-i)
+        time.sleep(0.01)
+
+    # stop pwm signal
+
+def cleanup():
+    servo1.stop()
+    servo2.stop()
+
+if __name__ == "__main__":
+    try:
+        happy_mode()
+    except KeyboardInterrupt:
+        cleanup()
